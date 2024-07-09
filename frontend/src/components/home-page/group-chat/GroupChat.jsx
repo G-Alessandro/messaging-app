741e1bd66dd4f37c128e1ac945879067a6a6b854
Alessandro <galloalessandro77@gmail.com>
2024-07-09 19:19:24 +0200

    Added GroupChat component

--- frontend/src/components/home-page/group-chat/GroupChat.jsx
+++ frontend/src/components/home-page/group-chat/GroupChat.jsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+
 export default function GeneralChat() {
+  const [groupChat, setGroupChat] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/group-chat", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          mode: "cors",
+        });
+        const data = await response.json();
+        if (!response.ok) {
+          setError(data.error);
+        } else {
+          setGroupChat(data.groupChat);
+        }
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-       <div>Group Chat</div>
+      {error && <p>{error}</p>}
+      {!groupChat && !error && <p>Loading group chat..</p>}
+      {groupChat && !error && (
+        <div>
+          <p>Group Chat</p>
+          {groupChat.groupChat.map((chat) => {
+            return (
+              <button key={chat.groupChatName}>
+                <p>{chat.groupChatName}</p>
+              </button>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
