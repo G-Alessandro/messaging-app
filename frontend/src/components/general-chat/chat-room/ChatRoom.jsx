76b273fc1a80f6ca6f6b62fa8656ccef6a3b8177
Alessandro <galloalessandro77@gmail.com>
2024-08-07 18:07:36 +0200

    Removed commented code

--- frontend/src/components/general-chat/chat-room/ChatRoom.jsx
+++ frontend/src/components/general-chat/chat-room/ChatRoom.jsx
@@ -5,10 +5,8 @@ import style from "./ChatRoom.module.css";
 export default function ChatRoom({ chatUserId, socket }) {
   const [error, setError] = useState(null);
   const [chatRoomData, setChatRoomData] = useState(null);
-  // const [messageInput, setMessageInput] = useState("");
   const [userData, setUserData] = useState(null);
   const [chatRoomUserData, setChatRoomUserData] = useState(null);
-  // const [previewUserImage, setPreviewUserImage] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -58,58 +56,6 @@ export default function ChatRoom({ chatUserId, socket }) {
     };
   }, [chatUserId]);
 
-  // const handleImageSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   try {
-  //     let image;
-
-  //     if (event.target["message-image"].files[0]) {
-  //       const formData = new FormData();
-  //       formData.append(
-  //         "message-image",
-  //         event.target["message-image"].files[0]
-  //       );
-
-  //       const response = await fetch(`http://localhost:3000/chat-room-image`, {
-  //         method: "POST",
-  //         credentials: "include",
-  //         mode: "cors",
-  //         body: formData,
-  //       });
-  //       const data = await response.json();
-
-  //       if (!response.ok) {
-  //         setError(data.error);
-  //       } else {
-  //         image = data.image;
-  //       }
-  //     }
-
-  //     if (messageInput.trim() !== "") {
-  //       const message = {
-  //         userId: userData._id,
-  //         userName: `${userData.firstName} ${userData.lastName}`,
-  //         timestamp: new Date(),
-  //         text: messageInput,
-  //         image,
-  //       };
-  //       socket.emit("send_message", { roomId: chatRoomData._id, message });
-  //       setMessageInput("");
-  //     }
-  //   } catch (err) {
-  //     setError(err);
-  //   }
-  // };
-
-  // const handleFileChange = (event) => {
-  //   const image = event.target.files[0];
-  //   if (image) {
-  //     const imageUrl = URL.createObjectURL(image);
-  //     setPreviewUserImage(imageUrl);
-  //   }
-  // };
-
   return (
     <div>
       {!chatRoomUserData && <p>Loading messages</p>}
@@ -149,32 +95,6 @@ export default function ChatRoom({ chatUserId, socket }) {
         chatRoomData={chatRoomData}
         userData={userData}
       />
-
-      {/* <div>
-        {previewUserImage && (
-          <div>
-            <button onClick={() => setPreviewUserImage(null)}>X</button>
-            <div>
-              <img className={style.chatRoomImg} src={previewUserImage} />
-            </div>
-          </div>
-        )}
-        <form onSubmit={handleImageSubmit}>
-          <input
-            type="file"
-            name="message-image"
-            id="message-image"
-            onChange={handleFileChange}
-          />
-          <input
-            type="text"
-            onChange={(e) => setMessageInput(e.target.value)}
-            value={messageInput}
-            placeholder="Type your message..."
-          />
-          <button type="submit">Send</button>
-        </form>
-      </div> */}
     </div>
   );
 }
