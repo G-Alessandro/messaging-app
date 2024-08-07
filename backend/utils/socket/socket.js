a7ced0e060e2e9d6078bfe71d5e59c42bc51852b
Alessandro <galloalessandro77@gmail.com>
2024-08-07 17:34:31 +0200

    Removed console.log from socket.js

--- backend/utils/socket/socket.js
+++ backend/utils/socket/socket.js
@@ -3,20 +3,15 @@ const Chat = require("../../models/chat");
 
 const socketHandler = (io) => {
   io.on("connection", (socket) => {
-    console.log("Nuovo client connesso");
-
     socket.on("user_connected", async ({ userId }) => {
       await UserAccount.findByIdAndUpdate(userId, { online: true });
-      console.log(`Utente ${userId} connesso`);
     });
 
     socket.on("join_room", async ({ userId, roomId }) => {
       socket.join(roomId);
-      console.log(`${userId} si è unito alla stanza ${roomId}`);
     });
 
     socket.on("send_message", async ({ roomId, message }) => {
-      
       io.to(roomId).emit("receive_message", message);
       const chat = await Chat.findById(roomId);
       chat.messages.push(message);
@@ -25,12 +20,9 @@ const socketHandler = (io) => {
 
     socket.on("user_disconnected", async ({ userId }) => {
       await UserAccount.findByIdAndUpdate(userId, { online: false });
-      console.log(`Utente ${userId} disconnesso`);
     });
 
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
+    socket.on("disconnect");
   });
 };
 
