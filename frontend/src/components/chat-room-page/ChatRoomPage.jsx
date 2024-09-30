ee205b549ccba58824fc121b6cf6334614cd8ebf
Alessandro <galloalessandro77@gmail.com>
2024-09-30 20:37:06 +0200

    Added navigate

--- frontend/src/components/chat-room-page/ChatRoomPage.jsx
+++ frontend/src/components/chat-room-page/ChatRoomPage.jsx
@@ -1,5 +1,5 @@
-import { useLocation } from "react-router-dom";
-import { useContext } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useContext } from "react";
 import { SocketContext } from "../../main.jsx";
 import ChatRoom from "../home-page/chat-room/ChatRoom.jsx";
 
@@ -7,6 +7,13 @@ export default function ChatRoomPage() {
   const location = useLocation();
   const { chatUserId } = location.state;
   const { socket } = useContext(SocketContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (socket === null) {
+      navigate("/");
+    }
+  }, [socket]);
 
   return <ChatRoom socket={socket} chatUserId={chatUserId} />;
 }
